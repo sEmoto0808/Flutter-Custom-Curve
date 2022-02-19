5dcf92253c25bff63f53dbe65540a4914300fc36
Sho Emoto <dd2d00seg1@gmail.com>
2022-02-20 02:20:56 +0900

    curve-2のパスを計算

--- custom_curves/lib/custom_clipper.dart
+++ custom_curves/lib/custom_clipper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-// NOTE: Curve Data: https://raw.githubusercontent.com/kamaleshbasu/bezier_animation/c71f0708c0ac4a4e92ed2b0b2d1d07babd31292f/adobe_xd/curve.svg
-// NOTE: Calc Curve: https://itchylabs.com/tools/path-to-bezier/
+/// NOTE: Curve Data: https://raw.githubusercontent.com/kamaleshbasu/bezier_animation/c71f0708c0ac4a4e92ed2b0b2d1d07babd31292f/adobe_xd/curve.svg
+/// NOTE: Calc Curve: https://itchylabs.com/tools/path-to-bezier/
 class BigClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
@@ -76,11 +76,72 @@ class BigClipper extends CustomClipper<Path> {
   }
 }
 
+/// NOTE: Curve Data: https://raw.githubusercontent.com/kamaleshbasu/bezier_animation/c71f0708c0ac4a4e92ed2b0b2d1d07babd31292f/adobe_xd/curve_final.svg
+/// NOTE: Calc Curve: https://itchylabs.com/tools/path-to-bezier/
 class SmallClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    // TODO: implement getClip
-    throw UnimplementedError();
+    Path path = Path();
+    final double _xScaling = size.width / 414;
+    final double _yScaling = size.height / 896;
+    path.lineTo(-0.003999999999997783 * _xScaling, 217.841 * _yScaling);
+    path.cubicTo(
+      -0.003999999999997783 * _xScaling,
+      217.841 * _yScaling,
+      19.14 * _xScaling,
+      265.91999999999996 * _yScaling,
+      67.233 * _xScaling,
+      265.91999999999996 * _yScaling,
+    );
+    path.cubicTo(
+      115.326 * _xScaling,
+      265.91999999999996 * _yScaling,
+      112.752 * _xScaling,
+      234.611 * _yScaling,
+      173.83299999999997 * _xScaling,
+      241.635 * _yScaling,
+    );
+    path.cubicTo(
+      234.914 * _xScaling,
+      248.659 * _yScaling,
+      272.866 * _xScaling,
+      301.691 * _yScaling,
+      328.608 * _xScaling,
+      301.691 * _yScaling,
+    );
+    path.cubicTo(
+      384.34999999999997 * _xScaling,
+      301.691 * _yScaling,
+      413.99600000000004 * _xScaling,
+      201.977 * _yScaling,
+      413.99600000000004 * _xScaling,
+      201.977 * _yScaling,
+    );
+    path.cubicTo(
+      413.99600000000004 * _xScaling,
+      201.977 * _yScaling,
+      413.99600000000004 * _xScaling,
+      0 * _yScaling,
+      413.99600000000004 * _xScaling,
+      0 * _yScaling,
+    );
+    path.cubicTo(
+      413.99600000000004 * _xScaling,
+      0 * _yScaling,
+      -0.003999999999976467 * _xScaling,
+      0 * _yScaling,
+      -0.003999999999976467 * _xScaling,
+      0 * _yScaling,
+    );
+    path.cubicTo(
+      -0.003999999999976467 * _xScaling,
+      0 * _yScaling,
+      -0.003999999999997783 * _xScaling,
+      217.841 * _yScaling,
+      -0.003999999999997783 * _xScaling,
+      217.841 * _yScaling,
+    );
+    return path;
   }
 
   @override
